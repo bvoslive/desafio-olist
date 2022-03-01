501ee4e100e3ea7140fdf4c63070eb591fc6c94a
Bruno Souza <b.vos@live.com>
2022-03-01 18:38:10 -0300

    Relacionando tfidf com a nota

--- EDA_olist.py
+++ EDA_olist.py
@@ -28,7 +28,7 @@ mas sim um compilado de emoções humanas
 * descritivo do produto
 * review equivocado
 * elogio
-    * entrega
+    * entrega (tempo)
     * vendedor
     * preço
     * produto
@@ -53,6 +53,7 @@ TF-IDF
 
 # IMPORTANDO BIBLIOTECAS
 from cgitb import enable
+import numpy as np
 import pandas as pd
 import os
 import glob
@@ -62,7 +63,9 @@ import spacy
 from tqdm import tqdm
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import DBSCAN
-
+import matplotlib.pyplot as plt
+import plotly
+import plotly.express as px
 
 componentes_excluir = ['ner', 'textcat', 'lemmatizer', 'senter', 'sentencizer', 'tagger', 'entity_linker', 'parser', 'entity_ruler', 'textcat_multilabel']
 nlp = spacy.load("pt_core_news_sm", exclude=componentes_excluir)
@@ -102,9 +105,8 @@ review_comment_message = review_comment_message.apply(lambda x: ''.join([letra f
 
 #ELIMINANDO BARRAS DE ESPAÇO LATERAIS
 review_comment_message = review_comment_message.str.strip()
-
 review_comment_message = review_comment_message.str.replace('\n', ' ')
-
+review_comment_message = review_comment_message.str.lower()
 
 
 # ELIMINANDO STOPWORDS
@@ -146,14 +148,128 @@ df_lista_stopwords_eliminados.to_csv('tratamento_stopwords.csv')
 # -----> TFIDF COM GEOLOCALIZAÇÃO <-----
 
 # MOSTRAR NO MAPA O QUE ESTÃO FALANDO POR REVIEW
+
+np.random.seed(42)
+
 df_geo = pd.read_csv('./datasets/raw/olist_geolocation_dataset.csv')
 reviews = pd.read_csv('./datasets/processed/tratamento_stopwords.csv')
+df_reviews = pd.read_csv('./datasets/raw/olist_order_reviews_dataset.csv', dtype='str')
+
+
 
 reviews.rename(columns={'Unnamed: 0': 'INDEX_ORIGINAL'}, inplace=True)
 reviews.set_index('INDEX_ORIGINAL', inplace=True)
+reviews = reviews['DOCS'].str.lower()
+reviews.dropna(inplace=True)
 
+reviews = reviews.apply(lambda x: ' '.join(pd.Series(x.split(' ')).unique().tolist()))
+
+
+
+#FILTRANDO REVIEWS
 reviews_index = reviews.index.tolist()
 
-df_geo = df_geo.iloc[reviews_index]
+df_reviews = df_reviews.loc[reviews_index]
 
-df_geo.iloc[10]
+
+
+df_geo = df_geo.iloc[reviews_index]
+df_geo['geolocation_city'].unique()
+
+lat_long = df_geo[['geolocation_lat', 'geolocation_lng']]
+
+sc = StandardScaler()
+
+X = sc.fit_transform(lat_long)
+
+#DBSCAN
+
+model = DBSCAN().fit(lat_long)
+labels = model.labels_
+pd.Series(list(labels)).unique()
+pd.Series(model.labels_).value_counts().loc[-1]
+lat_long.iloc[0]
+
+
+
+# KMEANS
+from sklearn.cluster import KMeans
+
+# NÚMERO DE ZONAS ATÉ A QUANTIDADE DE BAIRROS EM SÃO PAULO
+distortions = []
+K = range(5,95)
+for k in tqdm(K):
+    kmeanModel = KMeans(n_clusters=k)
+    kmeanModel.fit(X)
+    distortions.append(kmeanModel.inertia_)
+
+plt.figure(figsize=(16,8))
+plt.plot(distortions, 'bx-')
+plt.ylabel('Distortion')
+plt.title('The Elbow Method showing the optimal k')
+plt.show()
+
+
+kmeanModel = KMeans(n_clusters=15)
+kmeanModel.fit(X)
+
+coordenadas_ajustadas = kmeanModel.cluster_centers_
+centroides = sc.inverse_transform(coordenadas_ajustadas)
+
+
+
+
+# VISUALIZANDO GRÁFICO
+
+import plotly.graph_objects as go
+
+fig = go.Figure(data=go.Scattergeo(
+        lon = centroides[:, 1],
+        lat = centroides[:, 0],
+        #text = df['text'],
+        #mode = 'markers',
+        #marker_color = df['cnt'],
+        ))
+
+
+fig.update_layout(
+        title = 'Most trafficked US airports<br>(Hover for airport names)'
+    )
+
+fig.write_html("bairros.html")
+
+import geopandas as gpd
+
+import plotly.express as px
+px.set_mapbox_access_token(open(".mapbox_token").read())
+df = px.data.carshare()
+fig = px.scatter_mapbox(df, lat="centroid_lat", lon="centroid_lon",     color="peak_hour", size="car_hours",
+                  color_continuous_scale=px.colors.cyclical.IceFire, size_max=15, zoom=10)
+
+
+
+# TFIDF
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def tfidf(reviews):
+
+    vectorizer = TfidfVectorizer()
+    X = vectorizer.fit_transform(reviews)
+
+    termos = vectorizer.get_feature_names()
+    pontuacao = pd.DataFrame(X.mean(axis=0))
+    pontuacao = pontuacao.transpose()
+    pontuacao.columns = ['TFIDF']
+    pontuacao = pontuacao['TFIDF'].tolist()
+
+    tfidf_analise = pd.DataFrame({'termo': termos, 'tfidf': pontuacao})
+    tfidf_analise = tfidf_analise.sort_values('tfidf', ascending=False).iloc[:20]
+
+    return tfidf_analise
+
+# TFIDF COM PONTUAÇÃO
+
+index_nota = df_reviews[df_reviews['review_score'] == '2'].index.tolist()
+reviews_nota = reviews.loc[index_nota]
+
+tfidf(reviews_nota)
