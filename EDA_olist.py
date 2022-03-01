61066298a90c1d10114d8084b4a403075c1afcde
Bruno Souza <b.vos@live.com>
2022-03-01 02:34:14 -0300

    eliminando stopwords

--- EDA_olist.py
+++ EDA_olist.py
@@ -6,8 +6,6 @@ Dedicar seu tempo para fazer um elogio ou uma reclamação
 Representa um peso considerável a respeito da venda
 
 
-A partir de que momento o atraso se torna um problema?
-
 Quando não há comnetários
 Nesse quesito, os dados nulos podem significar algo
 
@@ -35,25 +33,44 @@ mas sim um compilado de emoções humanas
     * preço
     * produto
 
-
 A conquista do primeiro cliente
 O cliente comprou pela primeira vez e nunca mais comprou
 
 Churn, consumia bastante, depois parou de consumir
 o que esse cliente disse?
 
+quando um vendedor faz uma publicação
+a partir dos comentários
+como a venda se sucedeu depois?
+
+PROBLEMAS:
+A partir de que momento o atraso se torna um problema?
+TF-IDF
+
 
 """
 
 
 # IMPORTANDO BIBLIOTECAS
+from cgitb import enable
 import pandas as pd
 import os
 import glob
 import io
+import string
+import spacy
+from tqdm import tqdm
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import DBSCAN
+
+
+componentes_excluir = ['ner', 'textcat', 'lemmatizer', 'senter', 'sentencizer', 'tagger', 'entity_linker', 'parser', 'entity_ruler', 'textcat_multilabel']
+nlp = spacy.load("pt_core_news_sm", exclude=componentes_excluir)
+
+
 
 # EXPORTANDO FEATURES DE TODOS OS DATASETS
-datasets_arquivos = glob.glob('./datasets/*.csv')
+datasets_arquivos = glob.glob('./datasets/raw/*.csv')
 for dataset_arquivo in datasets_arquivos:
 
     df = pd.read_csv(dataset_arquivo)
@@ -70,10 +87,73 @@ for dataset_arquivo in datasets_arquivos:
 
 
 
+# -----> EXPLORANDO REVIEWS <-----
 
-#EXPLORANDO TEXTOS
-df_textos = pd.read_csv('./datasets/olist_order_reviews_dataset.csv')
-
-
+df_textos = pd.read_csv('./datasets/raw/olist_order_reviews_dataset.csv', encoding='utf-8')
 df_textos.dropna(subset=['review_comment_message'], inplace=True)
 df_textos.iloc[300]['review_comment_message']
+review_comment_message = df_textos['review_comment_message']
+
+
+# ELIMINANDO PONTUAÇÕES E BARRAS DE ESPAÇO
+pontuacoes = string.punctuation
+
+review_comment_message = review_comment_message.apply(lambda x: ''.join([letra for letra in x if letra not in pontuacoes]))
+
+#ELIMINANDO BARRAS DE ESPAÇO LATERAIS
+review_comment_message = review_comment_message.str.strip()
+
+review_comment_message = review_comment_message.str.replace('\n', ' ')
+
+
+
+# ELIMINANDO STOPWORDS
+def tipo_tokens_permitidos(token: str, tipos_permitidos = ['PROPN', 'NOUN', 'VERB', 'ADJ', 'INTJ', 'ADV']):
+
+    apenas_barras = [True if letra == ' ' else False for letra in token]
+    if all(apenas_barras):
+        return False
+
+    token = token.strip()
+    palavra_confere = nlp(token)[0].pos_  
+
+    for tipo_permitido in tipos_permitidos:
+        if palavra_confere == tipo_permitido:
+            #ENCONTROU O TIPO PERMITIDO
+            return True
+    #NÃO ENCONTROU O TPO PERMITIDO
+    return False
+
+
+def elimina_stopwords(doc):
+
+    doc = [token for token in doc.split(' ') if token != '']
+    doc = [token for token in doc if tipo_tokens_permitidos(token)]
+    doc = ' '.join(doc)
+
+    return doc
+
+review_comment_message_list = review_comment_message.tolist()
+
+lista_stopwords_eliminados = []
+for review in tqdm(review_comment_message_list):
+    lista_stopwords_eliminados.append(elimina_stopwords(review))
+
+df_lista_stopwords_eliminados = pd.DataFrame({'DOCS': lista_stopwords_eliminados})
+df_lista_stopwords_eliminados.index = review_comment_message.index.tolist()
+df_lista_stopwords_eliminados.to_csv('tratamento_stopwords.csv')
+
+# -----> TFIDF COM GEOLOCALIZAÇÃO <-----
+
+# MOSTRAR NO MAPA O QUE ESTÃO FALANDO POR REVIEW
+df_geo = pd.read_csv('./datasets/raw/olist_geolocation_dataset.csv')
+reviews = pd.read_csv('./datasets/processed/tratamento_stopwords.csv')
+
+reviews.rename(columns={'Unnamed: 0': 'INDEX_ORIGINAL'}, inplace=True)
+reviews.set_index('INDEX_ORIGINAL', inplace=True)
+
+reviews_index = reviews.index.tolist()
+
+df_geo = df_geo.iloc[reviews_index]
+
+df_geo.iloc[10]
