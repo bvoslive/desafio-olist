66990efd7b0ca266d7d258c521c83c5a3848260f
Bruno Vinicius de Oliveira e Souza <brunovos@uninove.br>
2022-03-02 21:33:47 -0300

    Exportando dados agrupados

--- EDA_olist.py
+++ EDA_olist.py
@@ -330,19 +330,11 @@ df_merged = df_merged[['geolocation_lat', 'geolocation_lng', 'customer_id']]
 
 df_merged.drop_duplicates(inplace=True)
 
-
 df_merged = df_merged.merge(df_orders, on='customer_id', how='right')
 df_merged = df_merged.merge(df_items, on='order_id', how='right')
 df_merged = df_merged.merge(df_reviews, on='order_id', how='right')
 
-
-
-df_merged.info()
-
 df_merged.dropna(subset=['order_delivered_customer_date'], inplace=True)
-df_merged
-
-
 
 df_merged['order_delivered_customer_date'] = pd.to_datetime(df_merged['order_delivered_customer_date'])
 df_merged['order_purchase_timestamp'] = pd.to_datetime(df_merged['order_purchase_timestamp'])
@@ -351,21 +343,22 @@ df_merged['order_estimated_delivery_date'] = pd.to_datetime(df_merged['order_est
 
 
 df_merged['tempo_entrega'] = df_merged['order_delivered_customer_date'] - df_merged['order_purchase_timestamp']
+df_merged['tempo_estimado_entrega'] = df_merged['order_estimated_delivery_date'] - df_merged['order_delivered_customer_date']
+
+
+df_merged['tempo_entrega'] = df_merged['tempo_entrega'].apply(lambda x: x.days)
+df_merged['tempo_estimado_entrega'] = df_merged['tempo_estimado_entrega'].apply(lambda x: x.days)
 
 
 
-
-df_merged.iloc[0]
-
-df_merged
-
-df_merged.iloc[0]
+df_merged_filtro = df_merged[['customer_id', 'geolocation_lat', 'geolocation_lng', 'price', 'freight_value', 'review_score', 'tempo_entrega', 'tempo_estimado_entrega']]
 
 
-df_orders
+df_merged_filtro_agrupado = \
+df_merged_filtro.groupby(['customer_id', 'geolocation_lat', 'geolocation_lng']) \
+                    .agg({'price': np.sum, 'freight_value':np.mean, 'review_score': np.mean, 'tempo_entrega': np.mean, 'tempo_estimado_entrega': np.mean})
 
-df_orders.iloc[0]
 
+df_merged_filtro_agrupado.reset_index(inplace=True)
 
-
-df_merged
+df_merged_filtro_agrupado.to_csv('data_grouped.csv', index=False)
